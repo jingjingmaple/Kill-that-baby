2bb3f2f2fc3f1159cd901f47a43cf2900dff9c38
Sorachat Patanasombat <jingjingmaple@gmail.com>
2017-12-18 03:04:56 +0700

    brick3

---
+++ core/src/com/jing/gdx/game/BrickRenderer.java
@@ -0,0 +1,30 @@
+package com.jing.gdx.game;
+
+import java.util.ArrayList;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class BrickRenderer {
+	private Brick brick;
+	private SpriteBatch batch;
+	private World world;
+	private Texture brickImg;
+	private ArrayList<Brick> bricklist = new ArrayList<Brick>();
+	public BrickRenderer(SpriteBatch batch,World world) {
+		this.batch = batch;
+		this.world = world;
+		brickImg = new Texture("brick.png");
+	}
+	public void addBrick(int x, int y) {
+		bricklist.add(new Brick(x,y, world));
+	}
+	public void render() {
+		batch.begin();
+		for (int n=0; n<bricklist.size(); n++) {
+			bricklist.get(n).update();
+			batch.draw(brickImg,bricklist.get(n).pos_x,bricklist.get(n).pos_y);
+		}
+		batch.end();
+	}
+}
